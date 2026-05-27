cd41fcb16ac5d20fe4aed5f84d40e02984aeea29
pavthecodes <pavlostheodoropouloss@gmail.com>
2026-05-28 01:15:26 +0300

    fix: path location line

--- src/main/java/org/ptcc/internals/Collections/Templates/MethodClassRule.java
+++ src/main/java/org/ptcc/internals/Collections/Templates/MethodClassRule.java
@@ -16,11 +16,11 @@ public abstract class MethodClassRule implements Rule {
     @Override
     public void check(Node node, List<Violation> violations) {
         if(!(node instanceof MethodDeclaration) || ((MethodDeclaration) node).isAbstract()) { // < Note the isAbstract
-            violations.add(new Violation.Builder("Node is not a method.", Severity.INFO).build());
+            violations.add(new Violation.Builder("Node is not a method.", Severity.INFO).at(node).build());
         }
         MethodDeclaration method = (MethodDeclaration) node;
         if(method.getBody().isEmpty()) {
-            violations.add(new Violation.Builder("Method is empty", Severity.INFO).build());
+            violations.add(new Violation.Builder("Method is empty", Severity.INFO).at(method).build());
         }
         analyze(node, violations);
     }
