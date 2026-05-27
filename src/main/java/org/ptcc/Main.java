32395db312bb5b73dcbf4d1c352dd09c7c8ce1ae
pavthecodes <pavlostheodoropouloss@gmail.com>
2026-05-28 01:20:17 +0300

    feat: integrate RuleChecker with violation output

--- src/main/java/org/ptcc/Main.java
+++ src/main/java/org/ptcc/Main.java
@@ -1,15 +1,16 @@
 package org.ptcc;
 
 
+import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Node;
 import org.ptcc.internals.Collections.Violation;
 import org.ptcc.internals.Rule;
-import org.ptcc.internals.Rules.EJGeneral.NamingConventionRule.EJRule68;
 import org.ptcc.internals.Rules.ImplementationScanner;
+import org.ptcc.internals.Rules.RuleChecker;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,25 +18,25 @@ import java.util.List;
 
 
 public class Main {
-    private static final String FILE_PATH = "src/main/java/org/ptcc/Main.java";
-    private static final String TEST_PATH = "src/main/java/org/ptcc/internals/Rules/EJObjects/EJRule2.java";
     private static final String MAIN_PATH = "src/main/java/org/ptcc/internals/Collections/Violation.java";
     public static void main(String[] args) throws IOException {
-        System.out.println(ImplementationScanner.getImplementations());
-         EJRule68 rule = new EJRule68();
-        testRuleNode(rule);
+        ParserConfiguration config = new ParserConfiguration();
+        config.setLanguageLevel(ParserConfiguration.LanguageLevel.JAVA_17);
+        StaticJavaParser.setConfiguration(config);
+        RuleChecker newCheck = new RuleChecker(ImplementationScanner.getRuleInstances());
+        List<Violation> violations = newCheck.runCheck(Path.of("src/main/java/org/ptcc/internals"));
+        violations.forEach(System.out::println);
+
+        
+
 
     }
     private static void testRuleNode(Rule rule) throws IOException {
         CompilationUnit cu = StaticJavaParser.parse(Paths.get(MAIN_PATH));
         List<Violation> violations = new ArrayList<>();
-
-        /*cu.findAll(Node.class).forEach(c -> {
-            rule.check(c, violations);
-        });*/
         rule.check(cu, violations);
 
-        violations.forEach(v -> System.out.println("  " + v.getMessage()));
+        violations.forEach(v -> System.out.println("  " + v.getMessage() + "\n"));
     }
 }
 
