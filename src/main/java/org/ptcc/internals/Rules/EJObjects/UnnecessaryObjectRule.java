a49cd5f3dd9d994b5d0e9d108fdafa59518c92c6
pavthecodes <pavlostheodoropouloss@gmail.com>
2026-06-02 23:30:21 +0300

    feat: unnecessary object rule

--- src/main/java/org/ptcc/internals/Rules/EJObjects/UnnecessaryObjectRule.java
+++ src/main/java/org/ptcc/internals/Rules/EJObjects/UnnecessaryObjectRule.java
@@ -1,50 +1,46 @@
 package org.ptcc.internals.Rules.EJObjects;
 
 import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.body.FieldDeclaration;
-import org.checkerframework.framework.type.AnnotatedTypeMirror;
+import com.github.javaparser.ast.expr.ObjectCreationExpr;
+import org.ptcc.internals.Collections.Severity;
 import org.ptcc.internals.Collections.Violation;
 import org.ptcc.internals.Rule;
 
 import java.util.List;
+import java.util.Set;
 
 /**
- * Work In Progress,
- * Rule cannot be created with pure AST searching, check Checker Framework for more information
- * @see <a href="https://checkerframework.org/api/org/checkerframework/framework/type/TypeHierarchy.html">
+ * Detects unnecessary object creation for wrapper types and String.
+ *
+ * Examples:
+ * - {new Integer(5)} should usually be {int}
+ * - {new Boolean(false)} should usually be {boolean}
+ * - {new String("text")} should usually be a string literal
  */
 class UnnecessaryObjectRule implements Rule {
-    private AnnotatedTypeMirror mirror;
-
-    public UnnecessaryObjectRule(AnnotatedTypeMirror mirror) {
-        this.mirror = mirror;
-    }
+    private static final Set<String> UNNECESSARY_OBJECT_TYPES = Set.of(
+            "Boolean",
+            "Byte",
+            "Character",
+            "Double",
+            "Float",
+            "Integer",
+            "Long",
+            "Short",
+            "String"
+    );
 
     @Override
     public void check(Node node, List<Violation> violations) {
-        List<FieldDeclaration> fields = node.findAll(FieldDeclaration.class);
-        fields.stream()
-                .forEach(f -> System.out.println());
-
-        }
+        node.findAll(ObjectCreationExpr.class).stream()
+                .filter(this::isUnnecessaryObjectCreation)
+                .forEach(expr -> violations.add(new Violation.Builder(
+                        "Unnecessary object creation for " + expr.getType().asString()
+                                + ". Use the primitive or literal form instead.",
+                        Severity.WARNING).at(expr).build()));
     }
-    //public boolean isWrapperType() {
-        //TypeMirror type = mirror.getUnderlyingType();
 
-
-// Avoid creating unnecessary objects, ex:
-// String name = new String("Anden");
-/*
-boolean primitives = fields.stream()
-                        .anyMatch(f -> {
-                            Class<?> type = f.getClass();
-                            return type.equals(Integer.class) ||
-                                    type.equals(Byte.class) ||
-                                    type.equals(Double.class) ||
-                                    type.equals(Boolean.class) ||
-                                    type.equals(BigDecimal.class) ||
-                                    type.equals(Character.class) ||
-                                    type.equals(Float.class) ||
-                                    type.equals(Long.class);
-                        });
- */
+    private boolean isUnnecessaryObjectCreation(ObjectCreationExpr expr) {
+        return UNNECESSARY_OBJECT_TYPES.contains(expr.getType().getNameAsString());
+    }
+}
