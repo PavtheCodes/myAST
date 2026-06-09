f51212bcbc2d53c18d50bf0b0a4a518968adb611
pavthecodes <pavlostheodoropouloss@gmail.com>
2026-06-09 12:08:27 +0300

    refactor: update fields to private and final

--- src/main/java/org/ptcc/internals/Rules/ImplementationScanner.java
+++ src/main/java/org/ptcc/internals/Rules/ImplementationScanner.java
@@ -10,12 +10,9 @@ import java.util.List;
 import java.util.Set;
 
 public class ImplementationScanner {
-    public static Reflections reflections = new Reflections("org.ptcc.internals.Rules");
-    public static Set<Class<? extends Rule>> implementations = reflections.getSubTypesOf(Rule.class);
+    private static final Reflections reflections = new Reflections("org.ptcc.internals.Rules");
+    private static final Set<Class<? extends Rule>> implementations = reflections.getSubTypesOf(Rule.class);
 
-    public static Set<Class<? extends Rule>> getImplementations() {
-        return implementations;
-    }
     public static List<Rule> getRuleInstances() {
         List<Rule> instances = new ArrayList<>();
 
