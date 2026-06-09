784778de94e0506222e20d0e2b74821927d773f1
pavthecodes <pavlostheodoropouloss@gmail.com>
2026-06-09 12:04:05 +0300

    feat: add combined solver to UnnecessaryObjectRule for proper type resolution

--- src/main/java/org/ptcc/internals/Rules/EJObjects/UnnecessaryObjectRule.java
+++ src/main/java/org/ptcc/internals/Rules/EJObjects/UnnecessaryObjectRule.java
@@ -1,47 +1,53 @@
 package org.ptcc.internals.Rules.EJObjects;
 
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 
+import org.ptcc.internals.Collections.Severity;
 import org.ptcc.internals.Collections.Violation;
 import org.ptcc.internals.Rule;
 
 import java.util.List;
 
-/**
- * Work In Progress,
- * Rule cannot be created with pure AST searching, check Checker Framework for more information
- * @see <a href="https://checkerframework.org/api/org/checkerframework/framework/type/TypeHierarchy.html">
- */
+
 class UnnecessaryObjectRule implements Rule {
-
-
-
     @Override
     public void check(Node node, List<Violation> violations) {
+        if (!(node instanceof CompilationUnit)) {
+            return;
+        }
+        enableCombinedSolver();
         List<FieldDeclaration> fields = node.findAll(FieldDeclaration.class);
-        fields.stream()
-                .forEach(f -> System.out.println());
+        for(FieldDeclaration fieldDeclaration : fields) {
+            String typeName = fieldDeclaration.getVariables().get(0).getType().asString();
+            if (typeName.equals("String") || typeName.equals("java.lang.String")) {
+                continue;
+            }
+            if(fieldDeclaration.getVariables().get(0).getType().isPrimitiveType()) {
+                continue;
+            }
+            if (fieldDeclaration.getVariables().get(0).getType().isReferenceType()) {
+                violations.add(new Violation.Builder("Unnecessary object creation", Severity.LOW).at(fieldDeclaration).build());
+            }
 
         }
+        disableCombinedSolver();
     }
-    //public boolean isWrapperType() {
-        //TypeMirror type = mirror.getUnderlyingType();
 
+    public void enableCombinedSolver() {
+        CombinedTypeSolver typeSolver = new CombinedTypeSolver();
+        JavaSymbolSolver javaSymbolSolver = new JavaSymbolSolver(typeSolver);
+        StaticJavaParser
+                .getParserConfiguration()
+                .setSymbolResolver(javaSymbolSolver);
+    }
 
-// Avoid creating unnecessary objects, ex:
-// String name = new String("Anden");
-/*
-boolean primitives = fields.stream()
-                        .anyMatch(f -> {
-                            Class<?> type = f.getClass();
-                            return type.equals(Integer.class) ||
-                                    type.equals(Byte.class) ||
-                                    type.equals(Double.class) ||
-                                    type.equals(Boolean.class) ||
-                                    type.equals(BigDecimal.class) ||
-                                    type.equals(Character.class) ||
-                                    type.equals(Float.class) ||
-                                    type.equals(Long.class);
-                        });
- */
+    public void disableCombinedSolver() {
+        StaticJavaParser.setConfiguration(new ParserConfiguration());
+    }
+}
